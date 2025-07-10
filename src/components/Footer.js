65895f448bda47c0f32b9e37afa2839fd00d8385
ramil reziapov <ramil6191@outlook.com>
2025-07-10 17:11:19 +0400

    Add basic structure for Footer, Header, Main, and Nav components
    - Implemented Footer component with copyright and policy links.
    - Created Header component with a simple header structure.
    - Added Main component as a placeholder for main content.
    - Developed Nav component with navigation links and logo.

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© 2023 Little Lemon. All rights reserved.</p>
+      <p>Privacy Policy | Terms of Service</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Nav.js
@@ -0,0 +1,29 @@
+function Nav() {
+  return (
+    <nav>
+      <img src="/LogoLL.svg" alt="Little Lemon Logo" />
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#menu">Menu</a>
+        </li>
+        <li>
+          <a href="#reservations">Reservations</a>
+        </li>
+        <li>
+          <a href="#order-online">Order Online</a>
+        </li>
+        <li>
+          <a href="#login">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
