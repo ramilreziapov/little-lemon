6c5c7b13307113173beffd68881b9b1542d4a6e5
ramil reziapov <ramil6191@outlook.com>
2025-07-13 18:42:14 +0400

    Refactor code structure for improved readability and maintainability

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ function App() {
   return (
     <>
       <div className="container">
+        <div className="space-left">space-left</div>
+        <div className="space-left-after">space-left-after</div>
         <div className="box-left">box-left</div>
+        <div className="box-left-after">box-left-after</div>
         <Nav />
         <Header />
         <Main />
         <Footer />
         <div className="box-right">box-right</div>
+        <div className="box-right-after">box-right-after</div>
+        <div className="space-right">space-right</div>
+        <div className="space-right-after">space-right-after</div>
       </div>
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,22 @@
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <section className="header-container">
+        <box className="item1">
+          <h1>Little Lemon</h1>
+          <p>Chicago</p>
+          <p>
+            We are a family-owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <button>Reserve a Table</button>
+        </box>
+        <box className="item2">
+          <img src="/restaurantfood.jpg" alt="Little Lemon Logo" />
+        </box>
+      </section>
+    </header>
+  );
 }
 
 export default Header;
