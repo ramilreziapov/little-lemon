83844241f6b2bf6b0c71a07b961faf652ceb0405
ramil reziapov <ramil6191@outlook.com>
2025-07-15 09:14:23 +0400

    Add lemon dessert image to the public directory

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
 function Header() {
   return (
     <header>
-      <section className="header-container">
-        <box className="item1">
-          <h1>Little Lemon</h1>
-          <p>Chicago</p>
-          <p>
-            We are a family-owned Mediterranean restaurant, focused on
-            traditional recipes served with a modern twist.
-          </p>
-          <button>Reserve a Table</button>
-        </box>
-        <box className="item2">
+      <section className="header-container1">
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>
+          We are a family-owned Mediterranean restaurant, focused on traditional
+          recipes served with a modern twist.
+        </p>
+        <button>Reserve a table</button>
+      </section>
+      <section className="header-container2">
+        <box className="img-box">
           <img src="/restaurantfood.jpg" alt="Little Lemon Logo" />
         </box>
       </section>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,67 @@
 function Main() {
-  return <main>Main</main>;
+  return (
+    <>
+      <main>
+        <section className="main-container">
+          <box className="main-container1">
+            <h2>Specials</h2>
+            <button>Online Menu</button>
+          </box>
+          <box className="main-container2">
+            <div className="item1">
+              <img src="/greek salad.jpg" alt="Greek Salad" />
+              <h3>
+                Greek Salad <span>$12.99</span>
+              </h3>
+              <p>
+                A delicious mix of fresh vegetables, feta cheese, and olives,
+                drizzled with olive oil.
+              </p>
+              <h3>
+                Order a delivery
+                <span>
+                  <img src="/delivery.png" alt="Delivery" />
+                </span>
+              </h3>
+            </div>
+            <div className="item1">
+              <img src="/bruschetta.svg" alt="Bruschetta" />
+              <h3>
+                Bruschetta <span>$8.99</span>
+              </h3>
+              <p>
+                Toasted bread topped with a mix of tomatoes, basil, and garlic,
+                served with balsamic glaze.
+              </p>
+              <h3>
+                Order a delivery
+                <span>
+                  <img src="/delivery.png" alt="Delivery" />
+                </span>
+              </h3>
+            </div>
+            <div className="item1">
+              <img src="/lemon dessert.jpg" alt="Lemon Dessert" />
+              <h3>
+                Lemon Dessert <span>$6.99</span>
+              </h3>
+              <p>
+                A refreshing lemon tart with a buttery crust, perfect for ending
+                your meal. It's a delightful way to cleanse your palate with a
+                sweet and tangy finish.
+              </p>
+              <h3>
+                Order a delivery
+                <span>
+                  <img src="/delivery.png" alt="Delivery" />
+                </span>
+              </h3>
+            </div>
+          </box>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default Main;
