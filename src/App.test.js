b2a0a47fdf0a66530f1e5eb7c5e9f92e1ddc8a36
ramil reziapov <ramil6191@outlook.com>
2025-07-21 12:06:18 +0400

    test: Add unit tests for BookingForm component, including heading, time selection, and submit button functionality

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import BookingForm from "./components/BookingForm";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Renders the BookingForm heading", () => {
+  render(<BookingForm />);
+
+  const headingElement = screen.getByText("First Name*");
+
+  expect(headingElement).toBeInTheDocument();
+});
+
+test("Selecting time update the input field time", () => {
+  render(<BookingForm />);
+
+  const timeSelect = screen.getByLabelText("Time*");
+  // Initial value should be empty
+  expect(timeSelect.value).toBe("");
+
+  fireEvent.change(timeSelect, { target: { value: "20:30" } });
+  expect(timeSelect.value).toBe("20:30");
+
+  // Check if the option is selected
+  const selectedOption = screen.getByRole("option", { name: "20:30" });
+  expect(selectedOption.selected).toBe(true);
+});
+
+test("Submitt button - renders and clicks", () => {
+  render(<BookingForm />);
+
+  const submitButton = screen.getByRole("button", { name: "Reserve a Table" });
+  expect(submitButton).toBeInTheDocument();
+  expect(submitButton).toBeEnabled();
+  expect(submitButton.type).toBe("submit");
 });
